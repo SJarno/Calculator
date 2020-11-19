3c56b8e23c48ee68c0742734657e6f6e095cc124
Heidi Holappa <73523507+heidi-holappa@users.noreply.github.com>
2020-11-19 20:08:03 +0200

    added some functions

--- src/main/resources/public/javascript/calculator.js
+++ src/main/resources/public/javascript/calculator.js
@@ -6,35 +6,47 @@ function setResult(result) {
     document.getElementById("result").innerHTML = result
 }
 
+function functionToString(functionToString) {
+    document.getElementById("functionToString").innerHTML = functionToString
+}
+
 function plus() {
     setResult(getNumber("a") + getNumber("b"))
+    functionToString(getNumber("a") + " + " + getNumber("b"))
 }
 
 function multiply() {
     setResult(getNumber("a") * getNumber("b"))
+    functionToString(getNumber("a") + " * " + getNumber("b"))
 }
 
 function minus() {
     setResult(getNumber("a") - getNumber("b"));
+    functionToString(getNumber("a") + " - " + getNumber("b"))
 }
  
 function divide() {
     setResult(getNumber("a") / getNumber("b"));
+    functionToString(getNumber("a") + " / " + getNumber("b"))
 }
 
 function exponent() {
     setResult(Math.pow(getNumber("a"), getNumber("b")));
+    functionToString(getNumber("a") + "^" + getNumber("b"))
 }
 
 function sin() {
     setResult(Math.sin(getNumber("c")));
+    functionToString("sin(" + getNumber("c") + ")")
 }
 
 function cos() {
     setResult(Math.cos(getNumber("c")));
+    functionToString("cos(" + getNumber("c") + ")")
 }
 
 function tan() {
     setResult(Math.tan(getNumber("c")));
+    functionToString("tan(" + getNumber("c") + ")")
 }
 
